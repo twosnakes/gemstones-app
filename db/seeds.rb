ea66fb09d9084a12b9c7e538a1bb8c5a69d040f9
Zoe Lembeck <zlembeck@gmail.com>
2017-08-31 15:52:57 -0500

    added show page and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+Stone.create!(name: "Amethyst", properties: "Amethyst crystal therapies are primarily associated with physical ailments of the nervous system, the curing of nightmares and insomnia, and balancing the crown chakra")
+Stone.create!(name: "Clear Quartz", properties: "Healing, Amplifies Energy")
+Stone.create!(name: "Turquoise", properties: "healing ailments of the immune, respiratory, waste and skeletal systems")
 
